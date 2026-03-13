5dc57281f5ee6f009b3d050d83a912e746d2cbe2
kylerisse <kylerisse@users.noreply.github.com>
2026-03-12 19:00:34 -0700

    fix macos timezone bug in aws_mfa

--- private_dot_config/private_fish/private_functions/private_awsutc_to_epoch.fish
+++ private_dot_config/private_fish/private_functions/private_awsutc_to_epoch.fish
@@ -1,4 +1,4 @@
 function awsutc_to_epoch
-    set -l unix_timestamp (date -j -f "%Y-%m-%dT%H:%M:%S+00:00" $argv[1] "+%s")
+    set -l unix_timestamp (TZ=UTC date -j -f "%Y-%m-%dT%H:%M:%S+00:00" $argv[1] "+%s")
     echo $unix_timestamp
 end
