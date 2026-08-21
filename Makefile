c1e210357f9ce3eab5b9aa801b6b8ac1b398a8ce
kylerisse <kylerisse@users.noreply.github.com>
2026-08-20 18:20:33 -0700

    add make lint / make fmt for fish files

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+FISH_FILES := $(shell find . -name "*.fish" -not -path "./.git/*")
+
 diff:
 	chezmoi diff
 
@@ -6,3 +8,10 @@ apply:
 
 status:
 	chezmoi status
+
+lint:
+	@for f in $(FISH_FILES); do fish -n $$f || exit 1; done
+	fish_indent --check $(FISH_FILES)
+
+fmt:
+	fish_indent -w $(FISH_FILES)
--- private_dot_config/private_fish/private_config.fish
+++ private_dot_config/private_fish/private_config.fish
@@ -1,51 +1,51 @@
 if status is-interactive
-  if type -q nvim
-    set EDITOR nvim
-    alias vi='nvim'
-    alias vim='nvim'
-  else if type -q vim
-    set EDITOR vim
-    alias vi='vim'
-  else
-    set EDITOR vi
-  end
+    if type -q nvim
+        set EDITOR nvim
+        alias vi='nvim'
+        alias vim='nvim'
+    else if type -q vim
+        set EDITOR vim
+        alias vi='vim'
+    else
+        set EDITOR vi
+    end
 
-  if type -q icdiff
-    alias diff='icdiff --line-numbers'
-  end
+    if type -q icdiff
+        alias diff='icdiff --line-numbers'
+    end
 
-  set LESS '-XR'
-  alias less='less -XR'
-  set PAGER 'less -XR'
+    set LESS -XR
+    alias less='less -XR'
+    set PAGER 'less -XR'
 
-  # private variables aren't secrets but shouldn't be public either
-  if test -e ~/.config/chezmoi/private-vars.fish
-    source ~/.config/chezmoi/private-vars.fish
-  end
+    # private variables aren't secrets but shouldn't be public either
+    if test -e ~/.config/chezmoi/private-vars.fish
+        source ~/.config/chezmoi/private-vars.fish
+    end
 
-  # direnv shell hooks
-  if type -q direnv
-    direnv hook fish | source
-  end
+    # direnv shell hooks
+    if type -q direnv
+        direnv hook fish | source
+    end
 
-  # kube shorthands
-  if type -q kubectl
-    alias k='kubectl'
-  end
+    # kube shorthands
+    if type -q kubectl
+        alias k='kubectl'
+    end
 
-  if type -q kubectx
-    alias kx='kubectx'
-  end
+    if type -q kubectx
+        alias kx='kubectx'
+    end
 
-  # podman shorthands
-  if type -q podman
-    alias docker='podman'
-    alias p='podman'
-    alias renovate-config-validator='podman run -v (pwd):/usr/src/app -ti ghcr.io/renovatebot/renovate renovate-config-validator'
-  end
+    # podman shorthands
+    if type -q podman
+        alias docker='podman'
+        alias p='podman'
+        alias renovate-config-validator='podman run -v (pwd):/usr/src/app -ti ghcr.io/renovatebot/renovate renovate-config-validator'
+    end
 
-  # set GOROOT to nix location
-  if test -e /run/current-system/sw/bin/go
-    set GOROOT (ls -al /run/current-system/sw/bin/go | awk '{print $11}' | string replace '/bin/' '/share/')
-  end
+    # set GOROOT to nix location
+    if test -e /run/current-system/sw/bin/go
+        set GOROOT (ls -al /run/current-system/sw/bin/go | awk '{print $11}' | string replace '/bin/' '/share/')
+    end
 end
